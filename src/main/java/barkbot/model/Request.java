1b53b06199e0cf8648ce6463208bc7b2e3f97c86
jacob Malter <jacobdmalter@gmail.com>
2018-09-11 20:22:40 -0700

    Reverted request object to work.

--- src/main/java/barkbot/model/Request.java
+++ src/main/java/barkbot/model/Request.java
@@ -1,19 +1,10 @@
 package barkbot.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
-import lombok.NonNull;
-import lombok.Value;
+import lombok.Data;
 
-@Value
+@Data
 @Builder
 public class Request {
-    @NonNull
-    private final String body;
-
-    @JsonCreator
-    public Request(@JsonProperty("body") @NonNull final String body) {
-        this.body = body;
-    }
+    private String body;
 }
